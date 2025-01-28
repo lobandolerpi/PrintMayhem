89b2e10d4207383fc3d39c0575a78da203d5947c
pbvCalamot <pedro.bonilla@ieselcalamot.com>
2025-01-28 13:03:53 +0100

    helloPrints.py editat des de pbvCalamot

--- helloPrints.py
+++ helloPrints.py
@@ -1,5 +1,5 @@
 quiHaEditat = ["P.Bonilla"]
-desDOnSHaEditat = []
+desDOnSHaEditat = ["pbvCalamot"]
 opcionsMenu = ["Sortir","Printar editors de la branca"]
 
 whatToDoNext = -1  # Defineixo variable per entrar al while (i jugar a jocs con>
