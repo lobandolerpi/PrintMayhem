12d534e0a82d47ee0cb95c25846141f54a83b5cb
R3alityHD <sukhmandeep.singh.kaur@ieselcalamot.com>
2025-01-28 20:57:04 +0100

    Singh Sukhamandeep helloPrints.py

--- helloPrints.py
+++ helloPrints.py
@@ -1,5 +1,5 @@
-quiHaEditat = ["P.Bonilla"]
-desDOnSHaEditat = []
+quiHaEditat = ["S.Singh"]
+desDOnSHaEditat = [SSingh]
 opcionsMenu = ["Sortir","Printar editors de la branca","Printar branques origen"]
 
 whatToDoNext = -1  # Defineixo variable per entrar al while (i jugar a jocs con>
