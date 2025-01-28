b30b32cbbe23e55c335ec877cfeaf6c4a087d272
Alex Hernandez <ahernandezguerrero582@gmail.com>
2025-01-28 20:52:32 +0100

    commit del codi identificatiu

--- helloPrints.py
+++ helloPrints.py
@@ -2,6 +2,10 @@ quiHaEditat = ["A.Hernandez"]
 desDOnSHaEditat = ["AHernandez"]
 opcionsMenu = ["Sortir","Printar editors de la branca","Printar branques origen"]
 
+quiHaEditat = ["P.Bonilla"]
+desDOnSHaEditat = []
+opcionsMenu = ["Sortir","Printar editors de la branca","Printar branques origen"]
+
 whatToDoNext = -1  # Defineixo variable per entrar al while (i jugar a jocs con>
 while whatToDoNext != 0:   
    print("\n", "\n") # es cutre pero no vull importar res a aquest examen.
