12b1e0ac403ecc91c1c567254ec2d5865574c351
Alex Hernandez <ahernandezguerrero582@gmail.com>
2025-01-28 20:17:44 +0100

    primers canvis

--- helloPrints.py
+++ helloPrints.py
@@ -1,5 +1,5 @@
-quiHaEditat = ["P.Bonilla"]
-desDOnSHaEditat = []
+quiHaEditat = ["A.Hernandez"]
+desDOnSHaEditat = ["AHernandez"]
 opcionsMenu = ["Sortir","Printar editors de la branca","Printar branques origen"]
 
 whatToDoNext = -1  # Defineixo variable per entrar al while (i jugar a jocs con>
