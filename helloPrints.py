fc798b061df76ba9838332dd9eb44f6efad02bf6
lobandolerpi <bonilla.p@gmail.com>
2025-01-28 12:54:27 +0100

    Afegida funcionalitat i Pedro com a editor

--- helloPrints.py
+++ helloPrints.py
@@ -1,1 +1,30 @@
-print("Hello World!")
+quiHaEditat = ["P.Bonilla"]
+desDOnSHaEditat = []
+opcionsMenu = ["Sortir","Printar editors de la branca"]
+
+whatToDoNext = -1  # Defineixo variable per entrar al while (i jugar a jocs con>
+while whatToDoNext != 0:   
+   print("\n", "\n") # es cutre, pero no vull importar res a aquest examen.
+   print("Hello World!")
+
+   # printo les opcions disponible
+   for i in range(len(opcionsMenu)):
+      print(i , ": " , opcionsMenu[i])
+
+   # M'asseguro que la opció es pot passar a sencer, i si no deso -1 
+   try:
+      whatToDoNext = int(input("Introdueix la opció desitjada : "))
+   except:
+      print("Has de introduir un número sencer")
+      whatToDoNext = -1
+
+   # Que fa cada opcion ) 0 no fa res, pq ja surt.
+   if (whatToDoNext not in range(len(opcionsMenu))):
+      print("Opcio Inválida")
+
+   elif whatToDoNext == 1:
+      print(" ====================")
+      print(" Ha editat: ", end ="")
+      for editor in quiHaEditat:
+         print(editor, end = ", ")
+      print("\n","====================")
