b4a9e2069929d9ae4a67fdbc66c2b69bcd5be5bb
lobandolerpi <bonilla.p@gmail.com>
2025-02-11 19:48:23 +0100

    He afegit el meu nom a quiHaEditat i OnSHaEditat

--- helloPrints.py
+++ helloPrints.py
@@ -1,5 +1,5 @@
-quiHaEditat = ["P.Bonilla"]
-desDOnSHaEditat = []
+quiHaEditat = ["P.Bonilla", "ZZrama"]
+desDOnSHaEditat = ["ZZrama"]
 opcionsMenu = ["Sortir","Printar editors de la branca","Printar branques origen"]
 
 whatToDoNext = -1  # Defineixo variable per entrar al while (i jugar a jocs con>
